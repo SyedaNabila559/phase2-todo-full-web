908573f816f54ca87524cd9cdd3e70aa3224d950
SyedaNabila559 <abidkhan78000@gmail.com>
2026-01-15 03:44:19 +0500

    Added start.sh script to run backend

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
+
 #!/bin/bash
+
+# Backend folder mein jana
 cd backend
+
+# Dependencies install karna
 pip install -r requirements.txt
+
+# Uvicorn ke through FastAPI app start karna
 uvicorn main:app --host 0.0.0.0 --port $PORT
