80659fa7e40128d782fd4a42fd98ebd9756f41c1
SyedaNabila559 <abidkhan78000@gmail.com>
2026-01-15 15:25:50 +0500

    Fix Railway startup

--- start.sh
+++ start.sh
@@ -1,11 +1,6 @@
 
+
 #!/bin/sh
+set -e
+
 python -m uvicorn app.main:app --host 0.0.0.0 --port $PORT
-
-# Backend folder mein jana
-cd backend
-
-# Dependencies install karna
-pip install -r requirements.txt
-
-
